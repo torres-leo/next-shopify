08e7ec6a51c13d70891f7dd2380c1aee66af51d3
torres-leo <leojts24@gmail.com>
2024-12-26 13:33:40 -0600

    little footer XD

---
+++ src/components/shared/footer/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+	return (
+		<div className='py-10 bg-gradient-to-b from-white/10 to-black'>
+			<h6 className='text-4xl text-center font-semibold tracking-wide bg-gradient-to-b from-green-400 to-slate-950 from-1% bg-clip-text text-transparent'>
+				Next + Shopify Store
+			</h6>
+		</div>
+	);
+}
+
+export default Footer;
